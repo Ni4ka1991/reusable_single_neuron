e9d0133fae6e5f22b1e3d0db66d0693137771b64
Ni4ka1991 <astahova_nica@mail.ru>
2022-01-18 11:30:07 +0200

    First step. Arrange data in array numpy

--- app.py
+++ app.py
@@ -16,21 +16,6 @@ projects   = [ 50, 100, 150,   25, 50, 75,    50, 100, 150 ] #number
 earn       = [ 60, 70,  80,    30, 35, 40,    50, 60, 70   ] #k$/year
 
 
-#arrays
-#[ x    2*x        3*x   ]
-#[ x    2*x        3*x   ]
-#[ x   x + k     x + 2*k ]
-
-#[ 5,   10,  y1 ] #15
-#[ 100, 200, y2 ] #300
-#[ 120, 150, y3 ] #180
-
-
-#calculation of y1 = f(x1); y2 = f(x2) => f identical =>>>>> 
-
-#[ x,   2 * x,   3 * x   ]
-#[ x,  x + k,  x + 2 * k ]
-
 
 # NEURON LOGIC ######
 
@@ -55,31 +40,26 @@ def accuracy( E ):
 
 # ************ ######
 
-d1 = np.array( [ [5, 10, 15], [50, 100, 150], [60, 70, 80] ] )
-d2 = np.array( [ [5, 10, 15], [25, 50,  75 ], [30, 35, 40] ] )
-d3 = np.array( [ [2, 4,  6 ], [50, 100, 150], [50, 60, 70] ] )
-d_total = ( [ 
-               [ [5, 10, 15], [50, 100, 150], [60, 70, 80] ],
-               [ [5, 10, 15], [25, 50,  75 ], [30, 35, 40] ],
-               [ [2, 4,  6 ], [50, 100, 150], [50, 60, 70] ]
-          ] )
+experience_data = ( [ 
+               [            
+                 [5,  10,  15 ],
+                 [50, 100, 150],
+                 [60, 70,  80 ]
+               ],
+               
+               [ 
+                 [5,  10, 15],
+                 [25, 50, 75],
+                 [30, 35, 40]
+               ],
+               
+               [
+                 [2,  4,   6  ],
+                 [50, 100, 150],
+                 [50, 60,  70 ]
+               ]
+                    ] )
 
-
-
-a = np.ones(3)
-a2 = np.ones(( 3, 2 ))
-
-b = np.zeros(3)
-b2 = np.zeros(( 3, 2 ))
-c = np.random.random(3)
-std_c = c.std()
-mean_c = c.mean()
 system( "clear" )
-print( a )
-print( a2 )
-print( b )
-print( b2 )
-print( "#"*20 + f"\nResulting matrix:\n{d_total}\n" + "#"*20 )
-print( f"Random array c 3*1 = {c}" )
-print( f"Standard deviation c: {std_c}" )
-print( f"Arithmetic mean c: {mean_c}" )
+print( experience_data )
+
