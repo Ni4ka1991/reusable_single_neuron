daef9ae3069dccfb1be9a5c3fc89169680f44f8e
Ni4ka1991 <astahova_nica@mail.ru>
2022-01-19 11:23:42 +0200

    Add shape of array

--- app.py
+++ app.py
@@ -6,44 +6,44 @@ from os import system
 import matplotlib.pyplot as plt
 import random
 from neuron import *
+#from data1 import *
 from data import *
 
-
-
+#PREDICTED DATA 
 predicted_data = experience_data.copy()
+my_data_shape = predicted_data.shape #(сколько строк, сколько столбцов, сколько слоев )
+
+
+
 
 #biases initialization
 #weights   = np.zeros( 2 )
-weights   = np.ones( 2 )
+#weights   = np.ones( 2 )
+weights   = np.random.random( 2 )
+
 biases    = weights.copy()
-
-
-#PREDICTED DATA 
+#biases    = np.zeros( 2 )
+#biases    = np.ones( 2 )
 
 n1 = Neuron( weights[0], biases[0] )
 n2 = Neuron( weights[1], biases[1] )
 
-
-for k in range( 0 , 3 ):
-    for i in range( 0, 3 ):
+for k in range( 0 , my_data_shape[2]):
+    for i in range( 0, my_data_shape[0]):
         predicted_data[ k, i, 2 ] = np.array( n1.forward( predicted_data[ k, :, 0 ] ))[i] + np.array( n2.forward( predicted_data[ k, :, 1 ] ))[i]
         i += 1
     k += 1
 
 
-
-
 #                                                   слой  столбец
 #                                                     v     v
-experience_data_axis_Z = np.vstack(( experience_data[ 0, :, 2 ], experience_data[ 1, :, 2 ], experience_data[ 2, :, 2 ] ))
-predicted_data_axis_Z  = np.vstack((  predicted_data[ 0, :, 2 ],  predicted_data[ 1, :, 2 ],  predicted_data[ 2, :, 2 ] ))
-system( "clear" )
-print(  predicted_data_axis_Z )
-print( experience_data_axis_Z )
-
-
 experience_data_axis_Z = np.hstack(( experience_data[ 0, :, 2 ], experience_data[ 1, :, 2 ], experience_data[ 2, :, 2 ] ))
 predicted_data_axis_Z  = np.hstack((  predicted_data[ 0, :, 2 ],  predicted_data[ 1, :, 2 ],  predicted_data[ 2, :, 2 ] ))
+
+
+
+system( "clear" )
+print( my_data_shape )
 print(  predicted_data_axis_Z )
 print( experience_data_axis_Z )
 
