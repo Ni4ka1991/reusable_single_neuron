8ff351f2b50849e5f4cf0392a9ed6da366c77f80
Ni4ka1991 <astahova_nica@mail.ru>
2022-01-19 10:59:14 +0200

    Select data: Z and Z_predicted

--- app.py
+++ app.py
@@ -32,12 +32,20 @@ for k in range( 0 , 3 ):
 
 
 
-#predicted_data[ k, i, 2 ] = np.array( n1.forward( predicted_data[ k, :, 0 ] ))[i] + np.array( n2.forward( predicted_data[ k, :, 1 ] ))[i]
 
-
-
+#                                                   слой  столбец
+#                                                     v     v
+experience_data_axis_Z = np.vstack(( experience_data[ 0, :, 2 ], experience_data[ 1, :, 2 ], experience_data[ 2, :, 2 ] ))
+predicted_data_axis_Z  = np.vstack((  predicted_data[ 0, :, 2 ],  predicted_data[ 1, :, 2 ],  predicted_data[ 2, :, 2 ] ))
 system( "clear" )
-print(predicted_data)
+print(  predicted_data_axis_Z )
+print( experience_data_axis_Z )
+
+
+experience_data_axis_Z = np.hstack(( experience_data[ 0, :, 2 ], experience_data[ 1, :, 2 ], experience_data[ 2, :, 2 ] ))
+predicted_data_axis_Z  = np.hstack((  predicted_data[ 0, :, 2 ],  predicted_data[ 1, :, 2 ],  predicted_data[ 2, :, 2 ] ))
+print(  predicted_data_axis_Z )
+print( experience_data_axis_Z )
 
 
 
