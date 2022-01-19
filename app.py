6b4d1a1569456bd84853c0819b9d799ea4fc7eff
Ni4ka1991 <astahova_nica@mail.ru>
2022-01-19 10:09:33 +0200

    Refined code )))

--- app.py
+++ app.py
@@ -5,25 +5,11 @@ import math
 from os import system
 import matplotlib.pyplot as plt
 import random
-#from mpl_toolkits.mplot3d import Axes3D
-
-#fig = plt.figure()
-#ax  = plt.axes( projection = '3d' )
 
 #all_data
 experience = [ 5,  10,  15,    5,  10, 15,    2,  4,   6   ] #years
 projects   = [ 50, 100, 150,   25, 50, 75,    50, 100, 150 ] #number
 earn       = [ 60, 70,  80,    30, 35, 40,    50, 60,  70  ] #k$/year
-
-#arrays
-#[ x    2*x        3*x   ]
-#[ x    2*x        3*x   ]
-#[ x   x + k     x + 2*k ]
-
-#[ 5,   10,  y1 ] #15
-#[ 100, 200, y2 ] #300
-#[ 120, 150, y3 ] #180
-
 
 # NEURON LOGIC ######
 
@@ -78,7 +64,6 @@ weights   = np.ones( 2 )
 biases    = weights.copy()
 
 
-
 #PREDICTED DATA 
 
 n1 = Neuron( weights[0], biases[0] )
@@ -93,7 +78,6 @@ for k in range( 0 , 3 ):
 
 
 system( "clear" )
-
 print(predicted_data)
 
 
