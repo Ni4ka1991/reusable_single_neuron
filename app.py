3e14636ed95df57fbf95c51caf30dcfd0c624384
Ni4ka1991 <astahova_nica@mail.ru>
2022-01-19 10:05:08 +0200

    Tested. Fix errors in loops

--- app.py
+++ app.py
@@ -78,17 +78,6 @@ weights   = np.ones( 2 )
 biases    = weights.copy()
 
 
-#data extraction
-X_0_data = predicted_data[ 0, :, 0 ] 
-X_1_data = predicted_data[ 1, :, 0 ] 
-X_2_data = predicted_data[ 2, :, 0 ] 
-
-
-
-Y_0_data = predicted_data[ 0, :, 1 ] 
-Y_1_data = predicted_data[ 1, :, 1 ] 
-Y_2_data = predicted_data[ 2, :, 1 ] 
-
 
 #PREDICTED DATA 
 
@@ -96,22 +85,8 @@ n1 = Neuron( weights[0], biases[0] )
 n2 = Neuron( weights[1], biases[1] )
 
 
-#debugging code
-#predicted_data[ 0, 0, 2 ] = np.array( n1.forward( predicted_data[ 0, :, 0 ] ))[0] + np.array( n2.forward( predicted_data[ 0, :, 1 ] ))[0]
-#predicted_data[ 0, 1, 2 ] = np.array( n1.forward( predicted_data[ 0, :, 0 ] ))[1] + np.array( n2.forward( predicted_data[ 0, :, 1 ] ))[1]
-#predicted_data[ 0, 2, 2 ] = np.array( n1.forward( predicted_data[ 0, :, 0 ] ))[2] + np.array( n2.forward( predicted_data[ 0, :, 1 ] ))[2]
-
-#predicted_data[ 1, 0, 2 ] = np.array( n1.forward( predicted_data[ 1, :, 0 ] ))[0] + np.array( n2.forward( predicted_data[ 1, :, 1 ] ))[0]
-#predicted_data[ 1, 1, 2 ] = np.array( n1.forward( predicted_data[ 1, :, 0 ] ))[1] + np.array( n2.forward( predicted_data[ 1, :, 1 ] ))[1]
-#predicted_data[ 1, 2, 2 ] = np.array( n1.forward( predicted_data[ 1, :, 0 ] ))[2] + np.array( n2.forward( predicted_data[ 1, :, 1 ] ))[2]
-
-#predicted_data[ 2, 0, 2 ] = np.array( n1.forward( predicted_data[ 2, :, 0 ] ))[0] + np.array( n2.forward( predicted_data[ 2, :, 1 ] ))[0]
-#predicted_data[ 2, 1, 2 ] = np.array( n1.forward( predicted_data[ 2, :, 0 ] ))[1] + np.array( n2.forward( predicted_data[ 2, :, 1 ] ))[1]
-#predicted_data[ 2, 2, 2 ] = np.array( n1.forward( predicted_data[ 2, :, 0 ] ))[2] + np.array( n2.forward( predicted_data[ 2, :, 1 ] ))[2]
-
-
-for k in range( 0 , 2 ):
-    for i in range( 0, 2 ):
+for k in range( 0 , 3 ):
+    for i in range( 0, 3 ):
         predicted_data[ k, i, 2 ] = np.array( n1.forward( predicted_data[ k, :, 0 ] ))[i] + np.array( n2.forward( predicted_data[ k, :, 1 ] ))[i]
         i += 1
     k += 1
