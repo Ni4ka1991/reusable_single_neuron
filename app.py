77817357cc39199c82dc2cde0a4e2e6e570d18d7
Ni4ka1991 <astahova_nica@mail.ru>
2022-01-19 15:10:42 +0200

    Delta calculation

--- app.py
+++ app.py
@@ -8,59 +8,30 @@ import random
 from neuron import *
 #from data1 import *
 from data import *
+from mse_calculation import *
+
 
 #PREDICTED DATA 
-predicted_data = experience_data.copy()
 my_data_shape = predicted_data.shape #(nr of layers, nr of lines, nr of columns )
 
 #biases initialization
-#weights   = np.zeros( 2 )
+weights   = np.zeros( 2 )
 #weights   = np.ones( 2 )
 #weights   = np.random.random( 2 )
 #biases    = weights.copy()
-#biases    = np.zeros( 2 )
+biases    = np.zeros( 2 )
 #biases    = np.ones( 2 )
 
 
 #CALCULATIONS
 
-weights   = np.random.random( 2 )
-biases    = np.random.random( 2 )
+def find_mse( weights, biases ):
+    weights   = np.random.random( 2 )
+    biases    = np.random.random( 2 )
+    mse = mse_calculation( my_data_shape, weights, biases )
+    return mse
 
-def mse_calculation( array_shape, ):
-
-    n1 = Neuron( weights[0], biases[0] )
-    n2 = Neuron( weights[1], biases[1] )
-
-    for k in range( 0 , array_shape[0]):       #k = number of z axises
-
-        for i in range( 0, array_shape[1]):    #i = number of lines
-            predicted_data[ k, i, 2 ] = np.array( n1.forward( predicted_data[ k, :, 0 ] ))[i] + np.array( n2.forward( predicted_data[ k, :, 1 ] ))[i]
-            i += 1
-        k += 1
-
-
-    experience_data_axis_Z = np.hstack(( experience_data[ 0, :, 2 ],\
-                                         experience_data[ 1, :, 2 ],\
-                                         experience_data[ 2, :, 2 ]\
-                                         ))
-
-    predicted_data_axis_Z  = np.hstack(( predicted_data[ 0, :, 2 ], \
-                                         predicted_data[ 1, :, 2 ],\
-                                         predicted_data[ 2, :, 2 ]\
-                                         ))
-
-
-    E = error( experience_data_axis_Z, predicted_data_axis_Z )
-    L = loss( E )
-    return L
-
-
-mse1 = mse_calculation( my_data_shape )
-weights   = np.random.random( 2 )
-biases    = np.random.random( 2 )
-mse2 = mse_calculation( my_data_shape )
-delta = mse1 - mse2
+delta = abs( find_mse( weights, biases ) - find_mse( weights, biases ))
 
 print( delta )
 
---
+++ mse_calculation.py
@@ -0,0 +1,35 @@
+import numpy as np
+from os import system
+from neuron import *
+#from data1 import *
+from data import *
+
+predicted_data = experience_data.copy()
+
+def mse_calculation( array_shape, w, b ):
+
+    n1 = Neuron( w[0], b[0] )
+    n2 = Neuron( w[1], b[1] )
+
+    for k in range( 0 , array_shape[0]):       #k = number of z axises
+
+        for i in range( 0, array_shape[1]):    #i = number of lines
+            predicted_data[ k, i, 2 ] = np.array( n1.forward( predicted_data[ k, :, 0 ] ))[i] + np.array( n2.forward( predicted_data[ k, :, 1 ] ))[i]
+            i += 1
+        k += 1
+
+
+    experience_data_axis_Z = np.hstack(( experience_data[ 0, :, 2 ],\
+                                         experience_data[ 1, :, 2 ],\
+                                         experience_data[ 2, :, 2 ]\
+                                         ))
+
+    predicted_data_axis_Z  = np.hstack(( predicted_data[ 0, :, 2 ], \
+                                         predicted_data[ 1, :, 2 ],\
+                                         predicted_data[ 2, :, 2 ]\
+                                         ))
+
+
+    E = error( experience_data_axis_Z, predicted_data_axis_Z )
+    L = loss( E )
+    return L
