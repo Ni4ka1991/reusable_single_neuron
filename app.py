54d59179176f3c33b342c91bb2e35975325b3d58
Ni4ka1991 <astahova_nica@mail.ru>
2022-01-19 11:46:29 +0200

    MSE calculation

--- app.py
+++ app.py
@@ -6,18 +6,12 @@ from os import system
 import matplotlib.pyplot as plt
 import random
 from neuron import *
-from data1 import *
-#from data import *
+#from data1 import *
+from data import *
 
 #PREDICTED DATA 
 predicted_data = experience_data.copy()
 my_data_shape = predicted_data.shape #(nr of layers, nr of lines, nr of columns )
-print( my_data_shape )
-input( "hit enter" )
-
-
-
-
 #biases initialization
 #weights   = np.zeros( 2 )
 #weights   = np.ones( 2 )
@@ -31,24 +25,25 @@ n1 = Neuron( weights[0], biases[0] )
 n2 = Neuron( weights[1], biases[1] )
 
 for k in range( 0 , my_data_shape[0]):       #k = number of z axises
+
     for i in range( 0, my_data_shape[1]):    #i = number of lines
         predicted_data[ k, i, 2 ] = np.array( n1.forward( predicted_data[ k, :, 0 ] ))[i] + np.array( n2.forward( predicted_data[ k, :, 1 ] ))[i]
         i += 1
     k += 1
 
 
-#                                                   слой  столбец
-#                                                     v     v
 experience_data_axis_Z = np.hstack(( experience_data[ 0, :, 2 ], experience_data[ 1, :, 2 ], experience_data[ 2, :, 2 ] ))
 predicted_data_axis_Z  = np.hstack((  predicted_data[ 0, :, 2 ],  predicted_data[ 1, :, 2 ],  predicted_data[ 2, :, 2 ] ))
 
+E = error( experience_data_axis_Z, predicted_data_axis_Z )
+L = loss( E )
 
 
 system( "clear" )
-print(  predicted_data_axis_Z )
-print( experience_data_axis_Z )
-
-
+print( f"z_pred : {predicted_data_axis_Z}" )
+print( f"z_exper: {experience_data_axis_Z}" )
+print( f"list of errors: {E}" )
+print( f"MSE: {L}" )
 
 
 
